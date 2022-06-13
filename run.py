4232c8d597708bb95e88c84a8085f8df3356b906
ligervision <vigerlizion@gmail.com>
2022-06-12 21:29:58 -0500

    Thurs hw

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_context():
+    return {'db': db, 'Post': Post, 'User': User}
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
